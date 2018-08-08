d781a5c0a65f5a54e6575bc0683ebc75c7b5edcc
reroadnike <1531900560@qq.com>
2018-08-08 17:58:50 +0800

    修改密码放开权限

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -16,4 +16,37 @@ class IndexController extends CommonController {
         $this->display("Public/empty");
     }
 
+
+    //修改密码
+    public function checkPass() {
+        if(IS_POST) {
+            $password = I('post.passOne');
+            $id = $_SESSION['admin']['id'];
+            //$passTwo = I('post.passTwo');
+            $postdata['id'] = $id;
+            if($password) {
+                $postdata['mixs'] = mt_rand(1,999999);
+                $postdata['password'] = md5($password.$postdata['mixs']);
+            } else {
+                $this->ajaxReturn(array('err'=>2,'msg'=>"密码不能为空"));
+            }
+            //$postdata['password'] = md5($password.$user['mixs']);
+            //print_r($postdata);die;
+            $res = D('User')->editUser($postdata);
+            if($res) {
+                session('[destroy]');
+                $data = array('err'=>1,'msg'=>"密码修改成功");
+            } else {
+                $data = array('err'=>0,'msg'=>"密码修改失败");
+            }
+            $this->ajaxReturn($data);
+
+        } else {
+            $this->display("Public/checkPass");
+        }
+    }
+
+
+
+
 }
