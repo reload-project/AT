3aeff5e827c8c5e2fb0f29811e5641278bc02e5a
reroadnike <1531900560@qq.com>
2018-08-10 09:39:49 +0800

    尝试编写弹窗显示json转数据显示不成功

--- App/Home/Controller/TaskNumberController.class.php
+++ App/Home/Controller/TaskNumberController.class.php
@@ -21,8 +21,21 @@ class TaskNumberController extends CommonController {
             $v['task_num_id'] = $task[$v['task_num_id']];
             $v['pro_num_id'] = $project[$v['pro_num_id']];
             $v['add_time'] = date('Y-m-d H:i:s',$v['add_time']);
+            $v['number_str'] = mb_substr($v['number_str'],0,100);
         }
     	$this->assign('data',$data);
     	$this->display();
 	}
+
+
+    public function showInfo() {
+        $id = I('post.id');
+        $map['id'] = $id;
+        $taskInfo = D('TaskNumber')->get_one($map);
+        if($taskInfo) {
+            $taskInfo['number_str'] = json_decode($taskInfo['number_str']);
+            $data = array('err'=>1,'data'=>$taskInfo);
+            $this->ajaxReturn($data);
+        }
+    }
 }
